f14a191000ae4efe069b7061b2ebb0578d88995d
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-07-08 16:57:45 +0300

    Fix tests, part 1

--- generic_positions/tests/test_app/models.py
+++ generic_positions/tests/test_app/models.py
@@ -1,14 +1,14 @@
-"""Dummy models to be used in test cases of the ``generic_positions`` app."""
-from django.contrib.contenttypes import generic
+"""Dummy models to be used in test cases of the ``fields_positions`` app."""
+from django.contrib.contenttypes import fields
 from django.db import models
 
 
 class DummyParentModel(models.Model):
-    """Dummy to be used in test cases of the ``generic_positions`` app."""
+    """Dummy to be used in test cases of the ``fields_positions`` app."""
     name = models.CharField(max_length=256, blank=True)
 
-    generic_position = generic.GenericRelation(
-        'generic_positions.ObjectPosition'
+    fields_position = fields.GenericRelation(
+        'fields_positions.ObjectPosition'
     )
 
     def __unicode__(self):
@@ -16,9 +16,9 @@ class DummyParentModel(models.Model):
 
 
 class DummyModel(models.Model):
-    """Dummy to be used in test cases of the ``generic_positions`` app."""
+    """Dummy to be used in test cases of the ``fields_positions`` app."""
     name = models.CharField(max_length=256, blank=True)
-    parent = models.ForeignKey(DummyParentModel)
+    parent = models.ForeignKey(DummyParentModel, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return self.name
@@ -28,6 +28,6 @@ class DummyModel(models.Model):
 # the GenericRelation directly to your model if it is part of your own app.
 # The monkeypatching will only be used for models of third party apps.
 DummyModel.add_to_class(
-    'generic_position',
-    fields.GenericRelation('generic_positions.ObjectPosition'),
+    'fields_position',
+    fields.GenericRelation('fields_positions.ObjectPosition'),
 )
